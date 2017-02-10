343e61a5c31efa424d2faae3bad93809e093fbd8
Parra, Justin <parra.justin.main@gmail.com>
2017-02-09 18:09:55 -0700

    Base commit, nothing important

---
+++ info/index.php
@@ -0,0 +1,14 @@
+<?php
+
+$size = 10;
+$strategies = array("Smart", "Random", "Sweep");
+$
+
+echo "{\"size\":" + 
+
+echo {"size": 10,
+    "strategies": ["Smart", "Random", "Sweep"],
+        "ships": [
+	      {"name": "Aircraft carrier", "size": 5}, {"name": "Battleship", "size": 4}, {"name": "Frigate", "size": 3}, {"name": "Submarine", "size": 3}, {"name": "Minesweeper", "size": 2}]}
+
+?>
