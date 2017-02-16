d3b3f160ed612f86381b81c9d640841f0039be27
Parra, Justin <parra.justin.main@gmail.com>
2017-02-16 14:20:48 -0700

    check this

--- info/index.php
+++ info/index.php
@@ -10,34 +10,26 @@ error_reporting(E_ALL ^ E_NOTICE);
 </html>
 
 <?php
-var_dump(require("../common/flat.php"));
 require_once('../common/flat.php');
 $db = new flat('../data');
 $db->doc('setting')->insert(array(
   "size" => strval(10),
   "strategies" => join(',', array("Smart", "Random", "Sweep", "test")),
-  "ships" => join(',', array("battleship", "frigate", "submarine", "minesweeper", "aircraft carrier")), ));
+  "ships" => json_encode(array(
+    array("name" => "Aircraft carrier", "size" => 5),
+    array("name" => "Battleship", "size" => 4),
+    array("name" => "Frigate", "size" => 3),
+    array("name" => "Submarine", "size" => 3),
+    array("name" => "Minesweeper", "size" => 2),
+  )),
+));
 
 
 //TODO: $data = $db->doc('setting')->findall();
 $data = $db->doc('setting')->find("10", "size"); // set data to the query looking for the most recent settings insert
 // echo join(',', array_keys($db->doc('setting')->find("10", "size")));
 
-$size = $data['size']; // from data get the size
-$strategies = explode(',', $data['strategies']); // from data turn strategies string "a,b,c,d" = array("a", "b", "c", "d")
-
-// print all the way up till the strategies array
-printf( "{\"size\": %s, \"strategies\": [", strval($size));
-
-// print out all strategies
-for($i = 0; $i < sizeof($strategies); $i++) {
-       if( $i != 0) {
-       	   printf(" ,");
-       }
-       printf("\"%s\"", strval($strategies[$i]));
-}
-
-// TODO: print out all ships like strategies
-printf("], \"ships\": [output ships here]}");
+$data['strategies'] = explode(',', $data['strategies']); // from data turn strategies string "a,b,c,d" = array("a", "b", "c", "d")
+$data['ships'] = json_decode($data['ships']);
 
 ?>
