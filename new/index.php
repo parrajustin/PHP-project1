9310d259296bfc22bc37718378058041341a42ce
Sebezayala <sayalaurtaza@miners.utep.edu>
2017-02-18 16:03:34 -0700

    Just messing around trying to understand how this works. Added shitty board class and modified /new index.php

--- new/index.php
+++ new/index.php
@@ -1,4 +1,24 @@
 <?php
 
+require_once('../common/Board.php');
+
+$strat= $_GET['strategy'];
+$ships= $_GET['ships'];
+
+$ships=explode(';', $ships);
+
+
+$board=new Borad();
+
+for($i=0;$i<count($ships);$i++){
+	$board->insert($ships[$i]);
+}
+
+if ($board->isValid){
+	$id=uniqid();
+	echo '{"response": true, "pid": '.$id.'}';
+}
+
+
 
 ?>
