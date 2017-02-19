efc0a93a10057eaf467c6c25774bc7158f0563a7
Parra, Justin <parra.justin.main@gmail.com>
2017-02-18 21:06:30 -0700

    Added more comments to new/index.php
    - also got rid of some commented code in new/ships.php

--- new/index.php
+++ new/index.php
@@ -11,6 +11,9 @@
 
 <?php
 
+  /////////////////////////////////////////////////////////////////////
+  // Checks to make sure the get for both ships and strategy are set //
+  /////////////////////////////////////////////////////////////////////
   if( !isset($_GET['ships']) || strlen($_GET['ships']) === 0) {
     echo json_encode(array(
       "response" => false,
@@ -25,28 +28,42 @@
     exit();
   }
 
-  // SETUP DATABASE
+  ////////////////////////////
+  // SET UP INITAL DATABASE //
+  ////////////////////////////
   require_once('../common/flat.php');
   require_once('../common/common.php');
   require_once('ships.php');
   $setup = new check();
   $setup->run();
   $db = new flat('../data');
-  // ===== END SETUP
 
-  // setup data variable
+  /////////////////////////
+  // setup data variable //
+  /////////////////////////
   $db->doc('setting');
+  /**
+   *   The settings data retrieved from the database, contains the following keys:
+   *   "size", "strategies", "ships", "id"
+   *   @var array
+   */
   $data = $db->find("10", "size");
-  $data['strategies'] = json_decode($data['strategies']);
+  $data['strategies'] = json_decode($data['strategies']); // json decode needs to be done to turn the strings into actuall arrays
   $data['ships'] = json_decode($data['ships']);
 
-  $ship_names = array(); // to keep track of avaliable ships
+  /**
+   *   Keeps track of the avaliable ships that can be places
+   *   @var array
+   */
+  $ship_names = array();
   foreach ($data['ships'] as $value) {
     $ship_names[] = (string) $value->name;
   }
   // ===== END data setup
 
-  // does the strategy even exist
+  ////////////////////////////////////////////////////////
+  // Does the strategy specified in the _get even exist //
+  ////////////////////////////////////////////////////////
   if( !in_array($_GET['strategy'], $data['strategies']) ) {
     echo json_encode(array(
       "response" => false,
@@ -55,7 +72,9 @@
     exit();
   }
 
-  // check ship playements
+  /////////////////////////////////////////////////
+  // are there even the correct number of ships? //
+  /////////////////////////////////////////////////
   $get_ships = explode(';', $_GET['ships']);
   if( sizeof($get_ships) != sizeof($data['ships']) ) { // are there the correct number of ships
     echo json_encode(array(
@@ -64,11 +83,21 @@
     ));
     exit();
   }
+
+  /**
+   *   Contains the player defined ships, used to store into database
+   *   @var array
+   */
   $ship_storage = array();
 
-  // check each ship statement
+
+  /***********************************************************************************************
+   *   We need to check that every ship statement , 'name,col,row,dir', is correctly formed then *
+   *   we have to check if they are placed correctly on the board so that none of the Ships      *
+   *   go out of bounds or intersect each other                                                  *
+   ***********************************************************************************************/
   foreach ($get_ships as $value) {
-    $temp = explode(',', $value);
+    $temp = explode(',', $value); // exploded value of the ship statement, each statement is separted by a  ;
     if( sizeof($temp) != 4 ) { // does each ship statment have the correct number of variables name,x,y,dir
       echo json_encode(array(
         "response" => false,
@@ -77,15 +106,15 @@
       exit();
     }
 
+    // replace all + in a ship name with a space since htlm doesn't let you send spaces over urls
     str_replace("+", " ", $temp[0]);
-    $temp[1] = (int) $temp[1];
-    $temp[2] = (int) $temp[2];
+    $temp[1] = (int) $temp[1]; // cast the second string into an int
+    $temp[2] = (int) $temp[2]; // same as above
 
-    // =====
-    // checks to make sure each of the values are correct
-    // =====
-    // does this ship even exist
-    if( !in_array($temp[0], $ship_names) ) { // does each ship statment have the correct number of variables name,x,y,dir
+    //////////////////////////////////////////////////////////////////
+    // We need to make sure that each of the statements are correct //
+    //////////////////////////////////////////////////////////////////
+    if( !in_array($temp[0], $ship_names) ) { // Does this ship name exist? check it against the name from the $ship_names array
       echo json_encode(array(
         "response" => false,
         "reason" => "Unknown or Duplicate ship name, $temp[0]",
@@ -93,7 +122,7 @@
       exit();
     }
     // is the x cord a number within the range
-    if( $temp[1] < 1 || $temp[1] > $data['size'] ) { // does each ship statment have the correct number of variables name,x,y,dir
+    if( $temp[1] < 1 || $temp[1] > $data['size'] ) {
       echo json_encode(array(
         "response" => false,
         "reason" => "Invalid ship x position, $temp[1]",
@@ -101,7 +130,7 @@
       exit();
     }
     // is the y cord a number within the range
-    if( $temp[2] < 1 || $temp[2] > $data['size'] ) { // does each ship statment have the correct number of variables name,x,y,dir
+    if( $temp[2] < 1 || $temp[2] > $data['size'] ) {
       echo json_encode(array(
         "response" => false,
         "reason" => "Invalid ship y position, $temp[2]",
@@ -122,13 +151,19 @@
     }
 
     $ship_storage[$temp[0]] = array("name" => $temp[0], "col" => $temp[1], "row" => $temp[2], "dir" => $temp[3] ); // add this ship to storage to pass on
-    unset($ship_names[array_search($temp[0], $ship_names)]); // get rid of ship in ship names so we can't place it twice
+    /***********************************************************************************
+     *   Here on the unset we are deleting the entry from the ship_names array so that *
+     *   we don't place a ship with the same name twice                                *
+     ***********************************************************************************/
+    unset($ship_names[array_search($temp[0], $ship_names)]);
   }
 
-  // check all ships to see if any intersect or any go out of bounds
+  /////////////////////////////////////////////////////////////////////
+  // check all ships to see if any intersect or any go out of bounds //
+  /////////////////////////////////////////////////////////////////////
   $ship_checker = new Ships($data);
   foreach ($ship_storage as $key => $value) {
-    if( !$ship_checker->place($key, $value) ) {
+    if( !$ship_checker->place($key, $value) ) { // if any do intersect or go out of bounds return an error
       echo json_encode(array(
         "response" => false,
         "reason" => "Invalid Ship deployments, $key",
@@ -137,25 +172,39 @@
     }
   }
 
-  $pid = uniqid();
+  //////////////////
+  // PID CREATION //
+  //////////////////
+  $pid = uniqid(); // create the uniq pid
   $db->doc('games'); // set db pointer to the games table
   $pids = ["a"];
   while( sizeof($pids) != 0) $pids = $db->find($pid, "pid"); // make sure this pid is unique
+  //////////////////////
+  // END PID CREATION //
+  //////////////////////
 
-  $ship_checker->reset(); // get rid of checker so that we may make one for the computer's ships
+  /***************************************************************
+   *   Here we reset the ship check so that we may use it agian. *
+   *   This way we don't waste memory making two ship checkers   *
+   ***************************************************************/
+  $ship_checker->reset();
   $comp_ship_storage = array();
 
-  foreach ($ship_storage as $key => $value) {
+  foreach ($ship_storage as $key => $value) { // Create a copy of the ship_storage array for the computers
     $comp_ship_storage[$key] = array();
 
     foreach ($value as $key2 => $value2) {
       $comp_ship_storage[$key][$key2] = $value2;
     }
 
+    // randomize values
     $comp_ship_storage[$key]['col'] = mt_rand(1, $data['size']);
     $comp_ship_storage[$key]['row'] = mt_rand(1, $data['size']);
     $comp_ship_storage[$key]['dir'] = mt_rand(0,1) == 1;
   }
+  /////////////////////////////////////////////////////////////////////////////////////////////////
+  // We need to now check if the ships are placed correctly, if not ranomize them until they are //
+  /////////////////////////////////////////////////////////////////////////////////////////////////
   foreach ($comp_ship_storage as $key => $value) {
     while(!$ship_checker->place($key, $comp_ship_storage[$key])) {
       $comp_ship_storage[$key]['dir'] = mt_rand(0,1) == 1;
@@ -164,7 +213,9 @@
     }
   }
 
-  // create the array to insert into the database
+  //////////////////////////////////////////////////
+  // create the array to insert into the database //
+  //////////////////////////////////////////////////
   $db_game_insert = array(
     "pid" => $pid,
     "strategy" => $_GET['strategy'],
@@ -175,6 +226,9 @@
 
   $db->insert($db_game_insert); // insert this game into the database
 
+  //////////////////////////////////////////
+  // finally return the accepted response //
+  //////////////////////////////////////////
   echo json_encode(array(
     "response" => true,
     "pid" => $pid,
