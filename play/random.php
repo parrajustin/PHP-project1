7ff22c40a7b88f5ba3e75a738c36beea49858318
jarshys <jarshys@gmail.com>
2017-02-21 15:34:57 -0700

    Revert "Merge remote-tracking branch 'refs/remotes/origin/Sebastian'"
    This reverts commit 9a9e05ac439b1f81550ba0cbd6e70ea86c80f2ff.

--- play/random.php
+++ play/random.php
@@ -1,69 +1,43 @@
 <?php
 /**
  *   Random class, should just shoot at random locations
-*/
+ */
 class Random {
 
-	/**
-	 *   The variable that holds the data for the players ships
-	 *   @var array
-	 */
-	private $player_ships = Null;
-	/**
-	 *   Variable that holds the data for the computer ships
-	 *   @var array
-	 */
-	private $computer_ships = Null;
-	/**
-	 *    Holds the shots fired in a key value pairs with "2,3" => "0,1" where 2 = x, 3 = y and 1 = computer fired here
-	 *   @var [type]
-	 */
-	private $shots = Null;
+  /**
+   *   The variable that holds the data for the players ships
+   *   @var array
+   */
+  private $player_ships = Null;
+  /**
+   *   Variable that holds the data for the computer ships
+   *   @var array
+   */
+  private $computer_ships = Null;
+  /**
+   *    Holds the shots fired in a key value pairs with "2,3" => "0,1" where 2 = x, 3 = y, 0 = human hasn't fired here and 1 = computer fired here
+   *   @var [type]
+   */
+  private $shots = Null;
 
-	/**
-	 *   Sets up this swep class
-	 *   @method __construct
-	 *   @param  array      $game_arry the data from the current game
-	 */
-	public function __construct($game_arry) {
-		// $this->player_ships = $game_arry['player'];
-		//$this->computer_ships = $game_arry['computer'];
-		$this->shots = $game_arry['computer_shots'];
-	}
+  /**
+   *   Sets up this swep class
+   *   @method __construct
+   *   @param  array      $game_arry the data from the current game
+   */
+  public function __construct($game_arry) {
+    $this->player_ships = $game_arry['player'];
+    $this->computer_ships = $game_arry['computer'];
+    $this->shots = $game_arry['shots'];
+  }
 
-
-
-	/**
-	 *   Returns where the next shot should be fired
-	 *   @method nextShot
-	 *   @return string   x,y coordinates where the computer should fire next
-	 */
-
-	public function nextShot() {
-		$x=rand(1,10);
-		$y=rand(1,10);
-			
-		//Check if the coordinates are valid//
-		$isValid=false;
-		while($isValid==false){
-			///First check if the shots array is empty//
-			//If it is add the shot to the array and exit the while//
-			if(empty($this->shots)){
-				$this->shots[$x.",".$y]=1;
-				break;
-			}
-			//If the shot already exist generate a second shot and check again//
-			if(array_key_exists($x.",".$y,$this->shots) ){
-				$x=rand(1,10);
-				$y=rand(1,10);
-				continue;
-			}
-			//If the shot is valid add it to the shots array and exit the while//
-			$isValid=true;
-			$this->shots[$x.",".$y]=1;
-		}
-			
-		return $x.",".$y;
-	}
+  /**
+   *   Returns where the next shot should be fired
+   *   @method nextShot
+   *   @return string   x,y coordinates where the computer should fire next
+   */
+  public function nextShot() {
+    return "3,4";
+  }
 }
 ?>
--- play/sweep.php
+++ play/sweep.php
@@ -1,67 +1,43 @@
 <?php
 /**
  *   Sweep class, should just shoot in a sweeping pattern
-*/
+ */
 class Sweep {
 
-	/**
-	 *   The variable that holds the data for the players ships
-	 *   @var array
-	 */
-	private $player_ships = Null;
-	/**
-	 *   Variable that holds the data for the computer ships
-	 *   @var array
-	 */
-	private $computer_ships = Null;
-	/**
-	 *    Holds the shots fired in a key value pairs with "2,3" => "1" where 2 = x, 3 = y and 1 = computer fired here
-	 *   @var [type]
-	 */
-	private $shots = Null;
+  /**
+   *   The variable that holds the data for the players ships
+   *   @var array
+   */
+  private $player_ships = Null;
+  /**
+   *   Variable that holds the data for the computer ships
+   *   @var array
+   */
+  private $computer_ships = Null;
+  /**
+   *    Holds the shots fired in a key value pairs with "2,3" => "0,1" where 2 = x, 3 = y, 0 = human hasn't fired here and 1 = computer fired here
+   *   @var [type]
+   */
+  private $shots = Null;
 
-	/**
-	 *   Sets up this swep class
-	 *   @method __construct
-	 *   @param  array      $game_arry the data from the current game
-	 */
-	public function __construct($game_arry) {
-		$this->player_ships = $game_arry['player'];
-		$this->computer_ships = $game_arry['computer'];
-		$this->shots = $game_arry['computer_shots'];
-	}
+  /**
+   *   Sets up this swep class
+   *   @method __construct
+   *   @param  array      $game_arry the data from the current game
+   */
+  public function __construct($game_arry) {
+    $this->player_ships = $game_arry['player'];
+    $this->computer_ships = $game_arry['computer'];
+    $this->shots = $game_arry['shots'];
+  }
 
-	/**
-	 *   Returns where the next shot should be fired
-	 *   @method nextShot
-	 *   @return string   x,y coordinates where the computer should fire next
-	 */
-	public function nextShot() {
-		//Check if the shots array is empty//
-		//If it is add the initial shot at 0,0 and return it//
-		if(empty($this->shots)){
-			$this->shots['0,0']=1;
-			return key($this->shots);
-		}
-		//Get the last shot from the shot array//
-		end($this->shots);
-		$key = key($this->shots);
-		list($x,$y)=explode(',', $key);
-		//Generate the next shot using the last shot coordinates//
-		//If the y coordinate is at the end of the board set it to 0 and increment the x coordinate//
-		if ($y==10){
-			$x++;
-			$this->shots[$x.",0"]=1;
-			end($this->shots);
-			$key = key($this->shots);
-			//Increment the y coordinate untill it reaches the end of the board///
-		}else {
-			$y++;
-			$this->shots[$x.",".$y]=1;
-			end($this->shots);
-			$key = key($this->shots);
-		}
-		return $key;
-	}
+  /**
+   *   Returns where the next shot should be fired
+   *   @method nextShot
+   *   @return string   x,y coordinates where the computer should fire next
+   */
+  public function nextShot() {
+    return "3,4";
+  }
 }
 ?>
