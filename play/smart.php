0af4600e4371258349bf3749120768b3cec2af07
jarshys <jarshys@gmail.com>
2017-02-21 22:20:38 -0700

    Smart strategy
    I try to create a smart strategy. I don't know if works or if i did
    right. I hope that changes is only in my branch and doesn't mess up the
    master branch

--- play/smart.php
+++ play/smart.php
@@ -37,7 +37,48 @@ class Smart {
    *   @return string   x,y coordinates where the computer should fire next
    */
   public function nextShot() {
-    return "3,4";
+  	
+  	$x=rand(1,$game->get_board_size());
+  	$y=rand(1,$game->get_board_size());
+  	
+  	//Check if the coordinates are valid//
+  	$isValid=false;
+  	while($isValid==false){
+  		///First check if the shots array is empty//
+  		//If it is add the shot to the array and exit the while//
+  		if(empty($this->shots)){
+  			$this->shots[$x.",".$y]=1;
+  			break;
+  		}
+  		//If the shot is not empty check if the coordates are equal to a ship position//
+  		if(!empty($this->shots)){
+  			//this if check if x is equal to the row of the ship if it is true 
+  			//add one to x to make a shot next to the previous 
+  			if((array_key_exists($x.",".$y,$this->shots )) && (computer_ship['row']==$x) ){
+  				$x=$x+1;
+  				continue;
+  			}
+  			//this if check if y is equal to the col of the ship if it is true
+  			//add one to y to make a shot next to the previous
+  			if((arra_key_exist($x.",".$y,$this->shots)) && (computer_ship['col']==$y)){
+  				$y=$y+1;
+  				continue;
+  			}
+  			//If x and y are not equal to a col or row from computer ship
+  			//Select a new random x and y
+  			else {
+  				$x=rand(1,$game->get_board_size());
+  				$y=rand(1,$game->get_board_size());
+  				continue;
+  			}
+  		}			
+  		//If the shot is valid add it to the shots array and exit the while//
+  		$isValid=true;
+  		$this->shots[$x.",".$y]=1;
+  	}
+  	
+  	return $x.",".$y;
+  	
   }
 }
 ?>
