574fe5c649eab95a7b4a1bd8264a31b5a073ef70
Sebezayala <sayalaurtaza@miners.utep.edu>
2017-02-22 22:19:18 -0700

    Modified the seep and under 34 strategies to work with the finished program

--- play/under34.php
+++ play/under34.php
@@ -9,110 +9,123 @@ class Under34 {
 	 *   @var array
 	 */
 	private $player_ships = Null;
-	/**
-	 *   Variable that holds the data for the computer ships
-	 *   @var array
-	 */
-	private $computer_ships = Null;
+
 	/**
 	 *    Holds the shots fired in a key value pairs with "2,3" => "0,1" where 2 = x, 3 = y and 1 = computer fired here
 	 *   @var [type]
 	 */
 	private $shots = Null;
 	/**
-	 *   Variable that holds all the coordinates for all the ships with the keys being the coordinates and the value being,
-	 *   0 if it has not been shot, 1 if it has been shot.
-	 *   @var array
+	 *   The game object from common
+	 *   @var game
 	 */
-	private $ship_placements=array();
-	/**
-	 *   Variable that indicates if the next shot should be a hit or miss.
-	 *   @var boolean
-	 */
-	private  $hit=false;
+	private $game = Null;
+
 	/**
 	 *   Sets up this under34 class
 	 *   @method __construct
 	 *   @param  array      $game_arry the data from the current game
 	 */
-	public function __construct($game_arry) {
-		$this->player_ships = $game_arry['player'];
-		$this->shots = $game_arry['computer_shots'];
-		
-		//Fill the ship placements array//
-		for($ship=0;$ship<count($this->player_ships);$ship++){
-			$this->ship_placements[$this->player_ships[$ship]['row'].','.$this->player_ships[$ship]['col']]= 0;
-			if ($this->player_ships[$ship]['dir']){
-				for($i=1;$i<$this->player_ships[$ship]['size'];$i++){
-					$this->ship_placements[$this->player_ships[$ship]['row'].','.($this->player_ships[$ship]['col']+$i)]=0;
-				}
-			}else {
-				for($i=1;$i<$this->player_ships[$ship]['size'];$i++){
-					$this->ship_placements[($this->player_ships[$ship]['row']+$i).','.$this->player_ships[$ship]['col']]=0;
-				}
-			}
-		}
+	public function __construct($game) {
+		$this->game = $game;
+		$this->shots = $game->get_computer_shots();
+		$this->player_ships = $game->get_player_ships();
+
 	}
-	
+
 	/**
 	 *   Generates a shot that is guaranteed to hit a ship
 	 *   @method hitShot
 	 */
 	public function hitShot(){
-		//Search through the ship_placements array for a valid coordinate//
-		reset($this->ship_placements);
-		for ($i=0;$i<count($this->ship_placements);$i++){
-			if ($this->ship_placements[key($this->ship_placements)]==0){
-				$this->shots[key($this->ship_placements)]=1;
-				$this->ship_placements[key($this->ship_placements)]=1;
+		$row=$col=$dir=null;
+		//Get the coordinates and direction of the first available ship that is alive//
+		foreach ($this->player_ships as $key => $value) {
+			if( $value['sunk'] !== $value['size'] ) {
+				$row=$value['row'];
+				$col=$value['col'];
+				$dir=$value['dir'];
 				break;
 			}
-			next($this->ship_placements);	
 		}
+
+		//Check the past shots to see where the next shot for that ship should be//
+		$isValid=false;
+		while($isValid == false){
+			if( $this->game->shot_exists($col, $row, 0) ){
+				if($dir){
+					$col++;
+				}else{
+					$row++;
+				}
+				continue;
+			}
+			$isValid = true;
+		}
+		return array($col, $row);
 	}
-	
+
 	/**
 	 *   Generates a shot that is guaranteed to miss
 	 *   @method missShot
 	 */
 	public function missShot(){
-		$x=rand(1,10);
-		$y=rand(1,10);
-		
+		$col = rand(1, $this->game->get_board_size());
+		$row = rand(1, $this->game->get_board_size());
+
 		//Check if the coordinates are valid//
 		$isValid=false;
 		while($isValid==false){
-			
+				
 			//If the generated shot already exist or it can hit a ship generate a second shot and check again//
-			if(array_key_exists($x.",".$y,$this->shots) ||  array_key_exists($x.",".$y,$this->ship_placements)){
-				$x=rand(1,10);
-				$y=rand(1,10);
+			if( $this->game->shot_exists($col, $row, 0) ){
+				$col = rand(1, $this->game->get_board_size());
+				$row = rand(1, $this->game->get_board_size());
 				continue;
 			}
-			//If the shot is valid add it to the shots array and exit the while//
+			foreach ($this->player_ships as $key => $value) {
+				if($this->shot_collision($value, $col, $row)){
+					$col = rand(1, $this->game->get_board_size());
+					$row = rand(1, $this->game->get_board_size());
+					continue;
+				}
+			}
 			$isValid=true;
-			$this->shots[$x.",".$y]=1;
+				
 		}
+		return array($col, $row);
 	}
-	
+
 	/**
 	 *   Returns where the next shot should be fired
 	 *   @method nextShot
 	 *   @return string   x,y coordinates where the computer should fire next
 	 */
 	public function nextShot() {
-		
-		if ($this->hit){
-			$this->hitShot();
-			$this->hit=false;
-		}else{
-			$this->missShot();
-			$this->hit=true;
-		}
-		
+		//Check if this is the first shot since the first shot will always be a miss//
 		end($this->shots);
-		return key($this->shots);
-		
+		if (key($this->shots)=="-1,-1"){
+			return $this->missShot();
+		}
+		//Check if the las shot hit a ship, if it did return a miss shot else return a hit shot//
+		list($col,$row)=explode(',', key($this->shots));
+		foreach ($this->player_ships as $key => $value) {
+			if($this->shot_collision($value, $col, $row)){
+				return $this->missShot();
+			}
+		}
+		return $this->hitShot();
 	}
+
+	private function shot_collision($rect1, $shot_col, $shot_row) {
+		if ($shot_col < $rect1['col'] + ($rect1['dir']? $rect1['size']: 1) &&
+				$shot_col + 1 > $rect1['col'] &&
+				$shot_row < $rect1['row'] + (!$rect1['dir']? $rect1['size']: 1) &&
+				1 + $shot_row > $rect1['row']) {
+					return true;
+				}
+				return false;
+	}
+
 }
 ?>
